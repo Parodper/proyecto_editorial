e99c07bd97c3592591b7270c46afc5436333ba87
Pablo <p.rperez@udc.es>
2024-10-25 16:32:53 +0200

    Busca recursiva polas ligazóns

--- practica/items.py
+++ practica/items.py
@@ -7,6 +7,7 @@ import scrapy
 
 
 class BookItem(scrapy.Item):
+    link = scrapy.Field()
     title = scrapy.Field()
     author = scrapy.Field()
     synopsis = scrapy.Field()
--- practica/spiders/planeta.py
+++ practica/spiders/planeta.py
@@ -11,21 +11,28 @@ class PlanetaSpider(scrapy.Spider):
     ]
 
     def parse_book(self, response) -> BookItem:
+        response = response.replace(body=response.body.decode('utf-8'))
+        isbn = response.xpath('//meta[@property="book:isbn"]/@content').get()
+        if isbn is None:
+            return None
         book = BookItem()
-        book['title'] = response.css(".titleProducto::text()")
+        book['title'] = response.css(".titleProducto::text").get()
         book['link'] = response.url
         book['author'] = response.xpath('//meta[@property="book:author"]/@content').get()
-        book['synopsis'] = response.css(".resumen-content::text").get()
+        book['synopsis'] = '\n'.join(response.css(".resumen-content *::text").getall())
         book['category'] = [x.get().strip() for x in response.css(".breadcrumbs").css("li").css("a::text")][1:]
-        book['isbn'] = response.xpath('//meta[@property="book:isbn"]/@content').get()
+        book['isbn'] = isbn
         book['publication_date'] = response.xpath('//meta[@property="book:release_date"]/@content').get()
+        return book
 
     def parse(self, response):
         match_book_url = re.compile("^/libro.*")
-
         for book in response.css("a::attr(href)"):
             url = book.get()
             if match_book_url.match(url) is not None:
                 next_page = response.urljoin(url)
-                yield scrapy.Request(next_page, callback=self.parse)
-
+                page = scrapy.Request(next_page, callback=self.parse_book)
+                if page is None:
+                    yield scrapy.Request(next_page, callback=self.parse)
+                else:
+                    yield page
